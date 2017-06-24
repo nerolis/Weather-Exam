1f9972d83041e210667d40e2822e152ca98eea96
nerolis <yakumoft@gmail.com>
2017-06-24 18:42:18 +0300

    Wind and humidity on map.

--- src/components/WeatherMap.js
+++ src/components/WeatherMap.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { main } from './main.scss'
+import { main } from './main.scss';
+import { Icon, Message } from 'semantic-ui-react';
+import {FormattedMessage } from 'react-intl';
+const AdditionalWeatherInfo = ({ weather, speed, humidity}) => <Icon name='marker' size='big' color='black'>
+                        <p>
+                        <FormattedMessage
+                        id={ 'Map.Speed' }
+                        defaultMessage={ 'Wind:' } />{speed}, 
+                        <FormattedMessage
+                        id={ 'Map.Humidity' }
+                        defaultMessage={ 'Humidity:' }/>{humidity}%
+                        </p>
+                        </Icon>;
+
+
 class WeatherMap extends React.Component {
   render() {
     const { weather } = this.props;
@@ -8,11 +22,18 @@ class WeatherMap extends React.Component {
      <div className='map' >
       <GoogleMapReact
       center={{lat: weather.coord.lat, lng: weather.coord.lon}} 
-      zoom={9}
+      zoom={10}
       >
+      <AdditionalWeatherInfo
+          lat={weather.coord.lat}
+          lng={weather.coord.lon}
+          speed={weather.wind.speed}
+          humidity={weather.main.humidity}
+        />
       </GoogleMapReact>
       </div>
     );
   }
 }
+
 export default WeatherMap;
